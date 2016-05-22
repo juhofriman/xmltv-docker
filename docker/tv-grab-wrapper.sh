42004da96513ba0f56cfc70bf00bfb7e78a40ae2
Juho Friman <juho.t.friman@gmail.com>
2016-05-22 18:34:14 +0300

    Added -a to that kludgy doctype removal
    To get this work on aws.

--- docker/tv-grab-wrapper.sh
+++ docker/tv-grab-wrapper.sh
@@ -3,5 +3,5 @@
 cd /opt/xmltv
 echo "$(date): Grabbing new data"
 tv_grab_fi --config-file /opt/xmltv/tv_grab_fi.conf --days 1 > /opt/xmltv/data/latest.xml
-cat /opt/xmltv/data/latest.xml | grep -v "DOCTYPE" > /data/tvdata.xml
+cat /opt/xmltv/data/latest.xml | grep -v -a "DOCTYPE" > /data/tvdata.xml
 echo "$(date): Done $(wc -l /data/tvdata.xml)"
